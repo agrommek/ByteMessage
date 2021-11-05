6e683e11ce519eaf1890d96d42d4cf6e23471a76
Andreas Grommek <76997441+agrommek@users.noreply.github.com>
2021-11-05 21:08:17 +0100

    removed dependency on netconv, re-implemented functionality locally

--- src/ByteMessageField.hpp
+++ src/ByteMessageField.hpp
@@ -31,7 +31,11 @@
  */
 
 #include <string.h>  // needed for memcpy()
-#include "netconv.h" // get it from https://github.com/agrommek/netconv
+/*
+ * Note: We re-implement a bit of functionality from the netconv library
+ * (see https://github.com/agrommek/netconv) in order to reduce external
+ * dependencies.
+ */
 
 /* common member function definitions */
 
@@ -76,50 +80,216 @@ ByteMessageField<T>& ByteMessageField<T>::operator= (const ByteMessageField<T> &
  * @details    The value is read from the underlying array.
  * @return     The value stored in the ByteMessageField object.
  */
- 
- 
+
 /** @cond method_specializations */
 
 /* function specializations for uint8_t */
-template<> void ByteMessageField<uint8_t>::set(uint8_t value) { *msgptr = value; }
-template<> uint8_t ByteMessageField<uint8_t>::get(void) const { return *msgptr; }
+
+template<> void ByteMessageField<uint8_t>::set(uint8_t value) {
+    *msgptr = value;
+}
+
+template<> uint8_t ByteMessageField<uint8_t>::get(void) const {
+    return *msgptr;
+}
 
 /* function specializations for int8_t */
-template<> void ByteMessageField<int8_t>::set(int8_t value) { *msgptr = static_cast<uint8_t>(value); }
-template<> int8_t ByteMessageField<int8_t>::get(void) const { return static_cast<int8_t>(*msgptr); }
+
+template<> void ByteMessageField<int8_t>::set(int8_t value) {
+    *msgptr = static_cast<uint8_t>(value);
+}
+
+template<> int8_t ByteMessageField<int8_t>::get(void) const {
+    return static_cast<int8_t>(*msgptr);
+}
 
 /* function specializations for uint16_t */
-template<> void ByteMessageField<uint16_t>::set(uint16_t value) { htons(value, msgptr); }
-template<> uint16_t ByteMessageField<uint16_t>::get(void) const { return ntohs(msgptr); }
+
+template<> void ByteMessageField<uint16_t>::set(uint16_t value) {
+//    htons(value, msgptr);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        value = __builtin_bswap16(value);
+    #endif
+    memcpy(msgptr, &value, sizeof(uint16_t));
+}
+
+template<> uint16_t ByteMessageField<uint16_t>::get(void) const {
+    //return ntohs(msgptr);
+    uint16_t v;
+    memcpy(&v, msgptr, sizeof(uint16_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap16(v);
+    #endif
+    return v;
+}
 
 /* function specializations for int16_t */
-template<> void ByteMessageField<int16_t>::set(int16_t value) { htons(static_cast<uint16_t>(value), msgptr); }
-template<> int16_t ByteMessageField<int16_t>::get(void) const { return static_cast<int16_t>(ntohs(msgptr)); }
+
+template<> void ByteMessageField<int16_t>::set(int16_t value) {
+//    htons(static_cast<uint16_t>(value), msgptr);
+    uint16_t v = static_cast<uint16_t>(value);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap16(v);
+    #endif
+    memcpy(msgptr, &v, sizeof(uint16_t));
+}
+
+template<> int16_t ByteMessageField<int16_t>::get(void) const {
+//    return static_cast<int16_t>(ntohs(msgptr));
+    uint16_t v;
+    memcpy(&v, msgptr, sizeof(uint16_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap16(v);
+    #endif
+    return static_cast<int16_t>(v);
+}
 
 /* function specializations for uint32_t */
-template<> void ByteMessageField<uint32_t>::set(uint32_t value) { htonl(value, msgptr); }
-template<> uint32_t ByteMessageField<uint32_t>::get(void) const { return ntohl(msgptr); }
+
+template<> void ByteMessageField<uint32_t>::set(uint32_t value) {
+//    htonl(value, msgptr);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        value = __builtin_bswap32(value);
+    #endif
+    memcpy(msgptr, &value, sizeof(uint32_t));
+}
+
+template<> uint32_t ByteMessageField<uint32_t>::get(void) const {
+//    return ntohl(msgptr);
+    uint32_t v;
+    memcpy(&v, msgptr, sizeof(uint32_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap32(v);
+    #endif
+    return v;
+}
 
 /* function specializations for int32_t */
-template<> void ByteMessageField<int32_t>::set(int32_t value) { htonl(static_cast<uint32_t>(value), msgptr);}
-template<> int32_t ByteMessageField<int32_t>::get(void) const { return static_cast<int32_t>(ntohl(msgptr)); }
+
+template<> void ByteMessageField<int32_t>::set(int32_t value) {
+//    htonl(static_cast<uint32_t>(value), msgptr);
+    uint32_t v = static_cast<uint32_t>(value);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap32(v);
+    #endif
+    memcpy(msgptr, &v, sizeof(uint32_t));
+}
+
+template<> int32_t ByteMessageField<int32_t>::get(void) const {
+//    return static_cast<int32_t>(ntohl(msgptr));
+    uint32_t v;
+    memcpy(&v, msgptr, sizeof(uint32_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap32(v);
+    #endif
+    return static_cast<int32_t>(v);
+}
 
 /* function specializations for uint64_t */
-template<> void ByteMessageField<uint64_t>::set(uint64_t value) { htonll(value, msgptr); }
-template<> uint64_t ByteMessageField<uint64_t>::get(void) const { return ntohll(msgptr); }
+
+template<> void ByteMessageField<uint64_t>::set(uint64_t value) {
+//    htonll(value, msgptr);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        value = __builtin_bswap64(value);
+    #endif
+    memcpy(msgptr, &value, sizeof(uint64_t));
+}
+
+template<> uint64_t ByteMessageField<uint64_t>::get(void) const {
+//    return ntohll(msgptr);
+    uint64_t v;
+    memcpy(&v, msgptr, sizeof(uint64_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap64(v);
+    #endif
+    return v;
+}
 
 /* function specializations for int64_t */
-template<> void ByteMessageField<int64_t>::set(int64_t value) { htonll(static_cast<uint64_t>(value), msgptr); }
-template<> int64_t ByteMessageField<int64_t>::get(void) const { return static_cast<int64_t>(ntohll(msgptr)); }
+
+template<> void ByteMessageField<int64_t>::set(int64_t value) {
+//    htonll(static_cast<uint64_t>(value), msgptr);
+    uint64_t v = static_cast<uint64_t>(value);
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap64(v);
+    #endif
+    memcpy(msgptr, &v, sizeof(uint64_t));
+}
+
+template<> int64_t ByteMessageField<int64_t>::get(void) const {
+//    return static_cast<int64_t>(ntohll(msgptr));
+    uint64_t v;
+    memcpy(&v, msgptr, sizeof(uint64_t));
+    #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap64(v);
+    #endif
+    return static_cast<int64_t>(v);
+}
 
 /* function specializations for float */
-template<> void ByteMessageField<float>::set(float value) { htonf(value, msgptr); }
-template<> float ByteMessageField<float>::get(void) const { return ntohf(msgptr); }
+
+template<> void ByteMessageField<float>::set(float value) {
+//    htonf(value, msgptr);
+    static_assert(sizeof(uint32_t) == sizeof(float));
+    #if ( __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        // convert to unsigned integer, byte-swap, copy to array
+        uint32_t v;
+        memcpy(&v, &value, sizeof(float));
+        v = __builtin_bswap32(v);
+        memcpy(msgptr, &v, sizeof(uint32_t));
+    #elif ( __FLOAT_WORD_ORDER__ == __ORDER_BIG_ENDIAN__ )
+        // direct copy of float to array
+        memcpy(msgptr, &value, sizeof(float));
+    #else
+        #error "float word order could not be determined!" 
+    #endif
+}
+
+template<> float ByteMessageField<float>::get(void) const {
+//    return ntohf(msgptr);
+    static_assert(sizeof(uint32_t) == sizeof(float));
+    uint32_t v;
+    memcpy(&v, msgptr, sizeof(float));
+    #if ( __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap32(v);
+    #endif
+    float f;
+    memcpy(&f, &v, sizeof(float));
+    return f;        
+}
 
 /* function specializations for double */
+
 #if (__SIZEOF_DOUBLE__ == 8)
-template<> void ByteMessageField<double>::set(double value) { htond(value, msgptr); }
-template<> double ByteMessageField<double>::get(void) const { return ntohd(msgptr); }
+template<> void ByteMessageField<double>::set(double value) {
+//    htond(value, msgptr);
+    static_assert(sizeof(uint64_t) == sizeof(double));
+    #if ( __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        // convert to unsigned integer, byte-swap, copy result to array
+        uint64_t v;
+        memcpy(&v, &value, sizeof(double));
+        v = __builtin_bswap64(v);
+        memcpy(msgptr, &v, sizeof(uint64_t));
+    #elif ( __FLOAT_WORD_ORDER__ == __ORDER_BIG_ENDIAN__ )
+        // direct copy of double to array
+        memcpy(msgptr, &value, sizeof(double));
+    #else
+        #error "float word order could not be determined!" 
+    #endif
+}
+
+template<> double ByteMessageField<double>::get(void) const {
+//    return ntohd(msgptr);
+    static_assert(sizeof(uint64_t) == sizeof(double));
+    uint64_t v;
+    memcpy(&v, msgptr, sizeof(double));
+    #if ( __FLOAT_WORD_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
+        v = __builtin_bswap64(v);
+    #endif
+    double d;
+    memcpy(&d, &v, sizeof(double));
+    return d;        
+}
 #endif
 
 /** @endcond */
